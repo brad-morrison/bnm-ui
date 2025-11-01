46269c975950c8aa11c0615cc65bdd05c1239ab8
Bradley Morrison <bradley_morrison@outlook.com>
2025-11-01 22:39:43 +0000

    Further setup

---
+++ packages/primitives/src/index.ts
@@ -0,0 +1,13 @@
+export * from './Box';
+export * from './Flex';
+export * from './Grid';
+export * from './GridItem';
+export * from './Stack';
+export * from './Container';
+export * from './SectionRoot';
+export * from './Heading';
+export * from './Text';
+export * from './Button';
+export * from './Input';
+
+// Add more exports as components are created
